f189b0b32b4c8be8cd65256fc15951d262696c5a
gustavo menezes <gustavo.menezes@rocketwave.tech>
2021-12-14 19:44:28 -0300

    Adds pagination

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,22 +10,37 @@ import { LoadingController } from '@ionic/angular';
 export class HomePage {
   searchWord = '';
   giphies = [];
+  offset = 0;
 
   constructor(
       private giphyService: GiphyService,
       private loadingCtrl: LoadingController
-  ) {}
+  ) {
+  }
+
+  async listGiphies(offset: number = 0, event = null) {
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    this.giphyService.getGiphies(this.searchWord, offset)
+        .subscribe((giphies) => {
+          if (offset !== 0) {
+            this.giphies = this.giphies.concat(giphies.data);
+            this.offset += giphies.pagination.count;
+            event.target.complete();
+          } else {
+            this.giphies = giphies.data;
+            this.offset = giphies.pagination.count;
+          }
+        }).add(() => loading.dismiss());
+  }
 
   async searchGIPHY() {
     if (this.searchWord.length > 0) {
-      const loading = await this.loadingCtrl.create();
-      await loading.present();
-      this.giphyService.getGiphies(this.searchWord)
-          .subscribe((giphies) => {
-            console.log(giphies.data);
-            this.giphies = giphies.data;
-            console.log(this.giphies[0].embed_url);
-          }).add(() => loading.dismiss());
+      this.listGiphies();
     }
+  }
+
+  scrollGiphies(event) {
+    this.listGiphies(this.offset, event);
   }
 }
